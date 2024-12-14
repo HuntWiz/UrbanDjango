a61b0324f2578d1d9ad6a661d4ad963d0aaff4a5
HuntWiz <botalov.gleb123@gmail.com>
2024-12-14 10:10:06 +0500

    md

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from UrbanDjango.example1.views import Index
+from UrbanDjango.task2.views import Index
 from django.views.generic import TemplateView
 
 urlpatterns = [
